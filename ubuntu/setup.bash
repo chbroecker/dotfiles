91c573f125207974e60ce6683e6b58722e4107c8
chbroecker <broecker.christopher@gmail.com>
2018-07-07 16:43:37 +0200

    change tmux.conf and add more setup for ubuntu

--- ubuntu/setup.bash
+++ ubuntu/setup.bash
@@ -22,15 +22,20 @@ dconf write /org/gnome/desktop/interface/gtk-theme '"Arc-Dark"'
 dconf write /org/gnome/desktop/interface/icon-theme '"Paper"'
 dconf write /org/gnome/desktop/interface/cursor-theme '"Paper"'
 
-# Installing Sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+# # Installing Sublime
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+# sudo apt-get install apt-transport-https
+# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# sudo apt-get update
+# sudo apt-get install sublime-text
 
 # Installing Atom
 curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
 sudo apt-get instal atom
+apm install file-icons
+
+# Installing Dotfiles
+git clone git@github.com:chbroecker/dotfiles.git .dotfiles
+ln -s ~/.dotfiles/ubuntu/.tmux.conf ~/
