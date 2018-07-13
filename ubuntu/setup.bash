4ed9ce9ef8ce90e3e2e7338dea6c9480d9d8d97b
chbroecker <broecker.christopher@gmail.com>
2018-07-13 06:51:19 +0200

    Add Spotify Installation

--- ubuntu/setup.bash
+++ ubuntu/setup.bash
@@ -36,6 +36,12 @@ sudo apt-get update
 sudo apt-get instal atom
 apm install file-icons
 
+# Installing Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
 # Installing Dotfiles
 git clone git@github.com:chbroecker/dotfiles.git .dotfiles
 ln -s ~/.dotfiles/ubuntu/.tmux.conf ~/
