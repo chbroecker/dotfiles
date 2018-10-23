2a9c642605c21463b08cc085a5f0627eff9aa9b3
Christopher Bröcker <chbroecker@uos.de>
2018-10-23 14:35:42 +0100

    add some brew installation for macOS

---
+++ macos/brew.sh
@@ -0,0 +1,19 @@
+## Installing brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install wget
+brew install tmux
+
+## Casks
+brew cask install macdown
+brew cask install firefox
+brew cask install rocket-chat
+brew cask install spotify
+brew cask install visual-studio-code
+brew cask install 1password
+brew cask install sublime
+
+## Lookat
+# mackup
+# macvim
