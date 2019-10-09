eac3d25b308c7b8afeaccd953a453a75dcbd21dd
Chris Broecker <broecker.christopher@gmail.com>
2019-10-09 11:35:18 +0200

    improve ubuntu setup

--- ubuntu/setup-server.bash
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-git clone https://github.com/chbroecker/dotfiles .dotfiles
-ln -s ~/.dotfiles/vim/.vimrc ~/
-ln -s ~/.dotfiles/tmux/.tmux.conf ~/
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-
